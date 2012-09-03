06ba8a5de2f42bc20ca53aa43afce97c57fc7e13
Kun Zhu <ariesyzhk@gmail.com>
2012-09-03 17:42:45 +0800

    submit by kun zhu.

---
+++ Sirius.Http.Tests/HttpConnectionTests.cs
@@ -0,0 +1,29 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="HttpConnectionTests.cs" company="Microsoft">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using NUnit.Framework;
+
+namespace Sirius.Http.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    [TestFixture]
+    public class HttpConnectionTests
+    {
+        [Test]
+        public void TestGet()
+        {
+            HttpConnection target = new HttpConnection();
+            Assert.NotNull(target.Get("http://kunzhu.co.cc"));
+        }
+    }
+}
---
+++ Sirius.Http/HttpConnection.cs
@@ -0,0 +1,51 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="HttpConnection.cs" company="Microsoft">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.IO;
+using System.Net;
+
+namespace Sirius.Http
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class HttpConnection
+    {
+        public string UserName { get; private set; }
+
+        public string Password { get; private set; }
+
+        private CookieContainer _cookies = new CookieContainer();
+
+        public HttpConnection(string userName = "", string password = "")
+        {
+            UserName = userName;
+            Password = password;
+        }
+
+        public Stream Get(string url)
+        {  
+            try
+            {
+                HttpWebRequest request = null;
+                request = HttpWebRequest.Create(url) as HttpWebRequest;
+                var response = request.GetResponse() as HttpWebResponse;
+                return response.GetResponseStream();
+            }
+            catch
+            {
+                return null;
+            }
+
+            return null;
+        }
+    }
+}
